491dcf6df203394a287113d7851ccd4653135319
JohnCletus001 <71078838+JohnCletus001@users.noreply.github.com>
2020-11-09 22:23:36 +0100

    Add files via upload

---
+++ project/js/main.js
@@ -0,0 +1,15 @@
+
+'use strict';
+
+(function ($) {
+
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function () {
+        $(".preloader").delay(1500).fadeOut("slow");
+    });
+    // /*------------------
+	// 	Date Picker
+	// --------------------*/
+})(jQuery);
